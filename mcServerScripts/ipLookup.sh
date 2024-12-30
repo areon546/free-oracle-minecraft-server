fa86faf02a6c0cd000f549550ee38b22f94f6e0a
areon546 <57325743+areon546@users.noreply.github.com>
2024-12-30 11:12:12 +0000

    Added ipLookup script, incomplete

---
+++ mcServerScripts/ipLookup.sh
@@ -0,0 +1,35 @@
+#! /bin/Bash
+
+
+if [ $# -ne 1 ]; then
+	echo "Error: Invalid number of arguments"
+    exit 1
+fi
+
+ip=$1
+
+ipValid () {
+    local ipv4Exp="([0-9]{3}[.]){3}[0-9]{3}"
+
+    numIps=$(echo "$1" | grep -E "$ipv4Exp" -c)
+
+    if [ $numIps = 1 ];then
+        result="ip valid"
+        return
+    else
+        result="ip not valid"
+        return
+    fi
+}
+
+ipValid $ip
+
+echo "asdas $result"
+
+. ./secrets.sh
+apiKey=$DRAGON_API_KEY
+. ./unsetSecrets.sh
+
+call="https://api.ipgeolocation.io/ipgeo?apiKey=$apiKey&ip=$ip"
+
+# curl $call
---
+++ mcServerScripts/unsetSecrets.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# unsets the global vars
+unset DRAGON_IP
+unset DRAGON_USERNAME
+unset DRAGON_PRIVATE_PATH
+unset DRAGON_API_KEY
+
+
+
+
+
+
+
+
+
+
+
